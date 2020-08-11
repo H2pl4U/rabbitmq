1c5372f2288dbfb97fc122a620ba9decd52a2654
liuwei <wei.liu@shmedo.cn>
2020-08-11 17:00:58 +0800

    topic

---
+++ routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/consumer/Consumer1.java
@@ -0,0 +1,7 @@
+package com.h2pl4u.rabbitmq.routing.topics.consumer;
+
+/**
+ * Created by Liuwei on 2020/8/11 16:54
+ */
+public class Consumer1 {
+}
---
+++ routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/provider/Provider4RoutingTopic.java
@@ -0,0 +1,7 @@
+package com.h2pl4u.rabbitmq.routing.topics.provider;
+
+/**
+ * Created by Liuwei on 2020/8/11 16:30
+ */
+public class Provider4RoutingTopic {
+}
