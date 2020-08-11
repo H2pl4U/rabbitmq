de952ea65255ee985900455c1b983ccf0b8b0313
liuwei <wei.liu@shmedo.cn>
2020-08-11 17:36:41 +0800

    topic

--- routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/consumer/Consumer2.java
+++ routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/consumer/Consumer2.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  *
  * Created by Liuwei on 2020/8/11 16:54
  */
-public class Consumer1 {
+public class Consumer2 {
     public static void main(String[] args) throws IOException {
         Connection connection = RabbitMQConfig.getConnection();
         Channel channel = connection.createChannel();
@@ -17,15 +17,14 @@ public class Consumer1 {
         String exchangeName = "topics";
         channel.exchangeDeclare(exchangeName, "topic");
         String queue = channel.queueDeclare().getQueue();
-        //绑定队列和交换机  动态通配符形式
-        channel.queueBind(queue,exchangeName,"user.*");
+        //绑定队列和交换机  动态通配符形式 #:任意单词组
+        channel.queueBind(queue,exchangeName,"user.#");
         //消费消息
         channel.basicConsume(queue,true,new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                System.out.println("消费者1："+new String(body));
+                System.out.println("消费者2："+new String(body));
             }
         });
-
     }
 }
--- routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/provider/Provider4RoutingTopic.java
+++ routing/src/main/java/com/h2pl4u/rabbitmq/routing/topics/provider/Provider4RoutingTopic.java
@@ -1,7 +1,27 @@
 package com.h2pl4u.rabbitmq.routing.topics.provider;
 
+import com.h2pl4u.rabbitmq.routing.config.RabbitMQConfig;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+
 /**
+ * Routing的订阅模型Topic  动态路由
+ * 本质上与Direct模型相比多了通配符
+ * *：匹配一个单词  #：匹配任意多个单词
  * Created by Liuwei on 2020/8/11 16:30
  */
 public class Provider4RoutingTopic {
+    public static void main(String[] args) throws IOException {
+        Connection connection = RabbitMQConfig.getConnection();
+        Channel channel = connection.createChannel();
+        String exchangeName = "topics";
+
+        channel.exchangeDeclare(exchangeName, "topic");
+        //发布消息
+        String routekey = "user.save.findAll";
+        channel.basicPublish(exchangeName, routekey, null, ("topic动态路由模型：[" + routekey + "]发送了消息").getBytes());
+        RabbitMQConfig.closeConnectionAndChannel(channel, connection);
+    }
 }
